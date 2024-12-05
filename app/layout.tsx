ba368a6e18d28ebe4417a64534c2f70d3e2aa086
Frosty7766 <mohamedahmed04321@gmail.com>
2024-12-05 22:55:44 +0200

    Container

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,23 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > <header>
-        <Link href={'/'}>Job Board</Link>
-      </header>
-      <nav>
-        <Link href={'/login'}>login</Link>
-        <Link href={'/post'}>post a job</Link>
-
-      </nav>
+      >
+        {" "}
+        <header>
+          <div className="container flex items-center justify-between py-4 px-6 mx-auto">
+            <Link href={"/"} className="font-bold text-xl">
+              Job Board
+            </Link>
+            <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
+              <Link className="bg-gray-200" href={"/login"}>
+                Login
+              </Link>
+              <Link className="bg-blue-600 text-white" href={"/new-listing"}>
+                Post a job
+              </Link>
+            </nav>
+          </div>
+        </header>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <dev>test</dev>
-  );
+  return <dev></dev>;
 }
