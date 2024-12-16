d47166ce59f58c0dfd359a8c1e866ab184a4e2e3
Ahmed Ashraf Helmy <ahmednassar3002@gmail.com>
2024-12-16 17:54:06 +0200

    authintcation 0.1

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,8 +1,6 @@
-import { getSignInUrl,withAuth } from "@workos-inc/authkit-nextjs";
 import Link from "next/link";
 
 export default async function Header() {
-  const { user } = await withAuth();
   return (
     <header>
       <div className=" flex items-center justify-between mx-auto ">
@@ -10,11 +8,9 @@ export default async function Header() {
           Job Board
         </Link>
         <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
-          {!user && (
           <Link className="bg-gray-200" href={"/login"}>
             Login
           </Link>
-          )}
           <Link className="bg-blue-600 text-white" href={"/new-listing"}>
             Post a job
           </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
 import Image from "next/image";
 import Hero from "./Components/Hero";
 import Jobs from "./Components/Jobs";
-import {
-  getSignInUrl,
-  getSignUpUrl,
-  withAuth,
-  signOut,
-} from '@workos-inc/authkit-nextjs';
 export default async function Home() {
 
  
-  const { user } = await withAuth();
-
- 
-  const signInUrl = await getSignInUrl();
-
- 
-  const signUpUrl = await getSignUpUrl(); 
-
   return (
     <>
       <Hero />
