bb3defffb748516224111e446a68880f67bcf737
Yousef Mohamed Abdelhady <yousef.mhady75@gmail.com>
2024-12-19 16:49:11 +0200

    Revert "lsa m5lsh"
    This reverts commit 3f16707a4ae561653e7b346b4cced55769e27867.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
 import Image from "next/image";
 import Hero from "./Components/Hero";
 import Jobs from "./Components/Jobs";
-import {
-  getSignInUrl,
-  getSignUpUrl,
-  withAuth,
-  signOut,
-} from '@workos-inc/authkit-nextjs';
-export default async function Home() {
 
- 
-  const { user } = await withAuth();
-
- 
-  const signInUrl = await getSignInUrl();
-
- 
-  const signUpUrl = await getSignUpUrl(); 
-
+export default function Home() {
   return (
     <>
       <Hero />
